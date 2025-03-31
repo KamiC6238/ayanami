475ba6f96d32d2450fb4717b442ec15efc51935a
kobayashi <823913645@qq.com>
2025-03-31 13:47:01 +0800

    chore: lint

--- src/store/config.ts
+++ src/store/config.ts
@@ -1,12 +1,20 @@
 import { DEFAULT_PIXEL_COLOR, DEFAULT_PIXEL_SIZE } from "@/constants";
 import { ToolTypeEnum } from "@/types";
-import { defineStore } from "pinia";
-import { ref } from "vue";
+import { defineStore, storeToRefs } from "pinia";
+import { ref, watch } from "vue";
+import { useColorPickerStore } from "./colorPicker";
 
 export const useConfigStore = defineStore("config", () => {
 	const toolType = ref<ToolTypeEnum>(ToolTypeEnum.Unknown);
 	const pixelSize = ref(DEFAULT_PIXEL_SIZE);
 	const pixelColor = ref(DEFAULT_PIXEL_COLOR);
+
+	const colorPickerStore = useColorPickerStore();
+	const { previewColor } = storeToRefs(colorPickerStore);
+
+	watch(previewColor, (val) => {
+		setPixelColor(val);
+	});
 
 	const setToolType = (_toolType: ToolTypeEnum) => {
 		toolType.value = _toolType;

38c5841b78169227186ad3244f4cc072004f3524
kobayashi <823913645@qq.com>
2025-04-05 19:09:20 +0800

    feat: display pixel background if the color has alpha

--- src/components/ColorPicker/Palette.vue
+++ src/components/ColorPicker/Palette.vue
@@ -32,14 +32,18 @@ const onPicked = (pickedColor: string, position: Position) => {
 <template>
   <div class="bg-[#7d929e] w-full h-full">
     <div class='bg-[#635561] w-full h-full  flex flex-wrap content-start'>
-      <div
-        v-for='[pickedColor, position] of pickedPalette'
-        class='w-7.5 h-7.5 border-1 border-solid border-black cursor-pointer'
-        :key='pickedColor'
-        :style='{ background: pickedColor }'
-        @click='() => onPicked(pickedColor, position)'
-      >
-    </div>
+      <div class='relative' v-for='[pickedColor, position] of pickedPalette'>
+        <div
+          class="absolute inset-0 bg-[url(@/assets/alpha-background.png)] bg-cover z-0 pointer-events-none"
+        ></div>
+        <div
+          class="relative w-7.5 h-7.5 border-1 border-solid border-black cursor-pointer"
+          :key="pickedColor"
+          :style="{ background: pickedColor }"
+          @click="() => onPicked(pickedColor, position)"
+        >
+        </div>
+      </div>
     </div>
   </div>
 </template>

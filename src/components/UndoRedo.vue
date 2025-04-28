49b9af56720bebbed09d0a6aeb50fcef75e0a33a
kobayashi <823913645@qq.com>
2025-04-28 17:27:18 +0800

    feat(undo & redo): support undo & redo with shortcut

--- src/components/UndoRedo.vue
+++ src/components/UndoRedo.vue
@@ -2,9 +2,33 @@
 import RedoIcon from "@/assets/icons/redo.svg";
 import UndoIcon from "@/assets/icons/undo.svg";
 import { useCanvasStore } from "@/store";
+import { type Subscription, fromEvent, tap } from "rxjs";
+import { onBeforeMount, onMounted, ref } from "vue";
 import { PixelBorderUltimate } from "./PixelBorder";
 
+const undoAndRedo$ = ref<Subscription | null>(null);
+
 const canvasStore = useCanvasStore();
+
+onMounted(() => {
+	undoAndRedo$.value = fromEvent<KeyboardEvent>(window, "keydown")
+		.pipe(
+			tap((event) => {
+				event.preventDefault();
+
+				const _ =
+					(event.ctrlKey || event.metaKey) && event.key.toLowerCase() === "z";
+				const isUndo = _ && !event.shiftKey;
+				const isRedo = _ && event.shiftKey;
+
+				isUndo && canvasStore.undo();
+				isRedo && canvasStore.redo();
+			}),
+		)
+		.subscribe();
+});
+
+onBeforeMount(() => undoAndRedo$.value?.unsubscribe());
 </script>
 <template>
   <div class="flex absolute bottom-2 right-2 gap-2">

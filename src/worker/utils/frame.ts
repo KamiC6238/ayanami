03b2962e55860637466b2957fbeb6c678e8a0f55
kobayashi <823913645@qq.com>
2025-05-24 18:15:44 +0800

    feat(frames): support switch frame

--- src/worker/utils/frame.ts
+++ src/worker/utils/frame.ts
@@ -1,27 +1,7 @@
-import { ToolTypeEnum } from "@/types";
-import * as recordUtils from "./record";
+import type { SwitchFrameMessagePayload } from "@/types";
 import * as renderUtils from "./render";
 
-interface ExportFrameSnapshot {
-	tabId: string;
-	frameId: string;
-}
-export const exportFrameSnapshot = (config: ExportFrameSnapshot) => {
-	const { tabId, frameId } = config;
-	const records = recordUtils.getUndoAndRedoStack(tabId);
-	const recordsWithFrameId = records.undoStack.filter((record) => {
-		const [toolType] = record;
-		const frameIndex = toolType === ToolTypeEnum.Eraser ? record[1] : record[2];
-		const _frameIndex = recordUtils.getFrameIndex(tabId, frameId);
-		return _frameIndex === frameIndex;
-	});
-
-	renderUtils.replayRecords(recordsWithFrameId, {
-		tabId,
-		frameId,
-		canvasType: "snapshot",
-	});
-
+const generateSnapshot = (tabId: string, frameId: string) => {
 	const canvas = renderUtils.getCanvas("snapshot");
 	canvas
 		?.convertToBlob({
@@ -38,4 +18,32 @@ export const exportFrameSnapshot = (config: ExportFrameSnapshot) => {
 				},
 			});
 		});
+};
+
+export const generateFrameSnapshot = (config: {
+	tabId: string;
+	frameId: string;
+}) => {
+	const { tabId, frameId } = config;
+	const records = renderUtils.getRecordsWithFrameId(tabId, frameId);
+
+	renderUtils.replayRecords(records, {
+		tabId,
+		frameId,
+		canvasType: "snapshot",
+	});
+
+	generateSnapshot(tabId, frameId);
+};
+
+export const switchFrame = (payload: SwitchFrameMessagePayload) => {
+	const { tabId, frameId } = payload;
+	const records = renderUtils.getRecordsWithFrameId(tabId, frameId);
+
+	renderUtils.clearAllPixels({ canvasType: "main" });
+	renderUtils.replayRecords(records, {
+		tabId,
+		frameId,
+		canvasType: "main",
+	});
 };

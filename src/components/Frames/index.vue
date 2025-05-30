c9c640898cf4e70885885f6a3e56a502a4e8aec6
kobayashi <823913645@qq.com>
2025-05-30 08:35:01 +0800

    feat(frames): add icon for active frame

--- src/components/Frames/index.vue
+++ src/components/Frames/index.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import ActiveFrameIcon from "@/assets/icons/active-frame.svg";
 import { useCanvasStore, useFramesStore } from "@/store";
 import { storeToRefs } from "pinia";
 import Frame from "./frame.vue";
@@ -10,14 +11,19 @@ const { frames, currentFrameId } = storeToRefs(framesStore);
 </script>
 <template>
   <div class='mr-2'>
-    <Frame
-      class='mb-2'
-      v-for='frameId of Object.keys(frames)'
-      :key='frameId'
-      :snapshot='frames[frameId].snapshot'
-      :active='currentFrameId === frameId'
-      @click='() => framesStore.switchFrame(frameId)'
-    />
+    <div v-for='frameId of Object.keys(frames)' class='relative' :key='frameId'>
+      <Frame
+        class='mb-2'
+        :snapshot='frames[frameId].snapshot'
+        :active='currentFrameId === frameId'
+        @click='() => framesStore.switchFrame(frameId)'
+      >
+      </Frame>
+      <ActiveFrameIcon
+        v-show='currentFrameId === frameId'
+        class='w-8 h-8 absolute top-[50%] left-[-35px] translate-y-[-50%]'
+      />
+    </div>
     <div
       class='text-[10px] cursor-pointer'
       @click='() => framesStore.createFrame(currentTabId)'

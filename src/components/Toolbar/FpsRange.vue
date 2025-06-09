0485f103a35be5d668d73bb7cc4855d80ea3ad13
kobayashi <kobayashi@kobayashideMacBook-Pro.local>
2025-06-10 07:19:06 +0800

    feat(frames): add fps range input

---
+++ src/components/Toolbar/FpsRange.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import RangeInput from "@/components/RangeInput.vue";
+import { useFramesStore } from "@/store";
+import { computed } from "vue";
+
+const framesStore = useFramesStore();
+
+const fps = computed(() => framesStore.fps);
+
+const onFpsChange = (e: Event) => {
+	framesStore.setFps(Number((e.target as HTMLInputElement).value));
+};
+</script>
+<template>
+  <RangeInput
+    class='ml-4'
+    :value="fps"
+    :min="1"
+    :max="24"
+    :step="1"
+    unit="fps"
+    @value-change="onFpsChange"
+  />
+</template>
